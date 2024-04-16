d507e8d2e5da66bca0ce86a29957d5eddeadd001
Rodrigo Araujo <rasch.dev@gmail.com>
2024-04-16 15:49:47 -0300

    Finalizado

--- src/application/MainTargetTest.java
+++ src/application/MainTargetTest.java
@@ -18,8 +18,11 @@ public class MainTargetTest {
 		else {
 			System.out.println("Resposta da questão 2: não faz parte da sequência Fibonacci.");
 		}
+		System.out.println();
 		
-		
+		System.out.print("Favor informar a palavra a inverter: ");
+		String original = scan.next();
+		System.out.println(questao3(original));
 	}
 
 	//  QUESTÃO 1
@@ -59,5 +62,34 @@ public class MainTargetTest {
 		return fazParte;
 	}
 	
+	/*
+	 * QUESTÃO 3
+	 * a) += 2;                 Próximo número = 9
+	 * b) *= 2;                 Próximo número = 128
+	 * c) = Math.pow(n, 2);     Próximo número = 49
+	 * d) ?;                    Próximo número = 100 (4x25)
+	 * e) fibonacci;            Próximo número = 13
+	 * f) ???
+	 */
 	
+	/*
+	 * QUESTÃO 4
+	 * Primeiro passo: ligar dois interruptores, de forma aleatória, e anotar os interruptores e as salas em que as lâmpadas foram ligadas
+	 * 
+	 * Segundo passo: manter aceso um dos dois interruptores que foram ligados antes, e alternar os dois interruptores (o 2° que foi ligado antes
+	 * e o que estava desligado) de estado. A sala que ligar a luz, nesse passo, é a que teve seu interruptor ligado no 2° passo,
+	 * e a lâmpada que desligar é a que teve seu interruptor ligado no 1° passo, mas desligado no segundo. A sala em que a luz se manteve ligada
+	 * após o 2° passo é aquela que não teve seu interruptor alternado no 2° passo.
+	 */
+	
+	//  QUESTÃO 5
+	public static String questao3(String original) {
+		String inversa = "";
+		
+		for (int i = 0; i < original.length(); i++) {
+			inversa = original.charAt(i) + inversa;
+		}
+		
+		return inversa;
+	}
 }
